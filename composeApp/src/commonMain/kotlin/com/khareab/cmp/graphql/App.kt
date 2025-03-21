e8598ddfba1cfdd6ef816335d4f1d47ef5226b7a
Abhiraj Khare <abhiraj.khare100@gmail.com>
2025-03-21 16:27:56 +0530

    feat: organise import

--- composeApp/src/commonMain/kotlin/com/khareab/cmp/graphql/App.kt
+++ composeApp/src/commonMain/kotlin/com/khareab/cmp/graphql/App.kt
@@ -1,24 +1,13 @@
 package com.khareab.cmp.graphql
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import cmpgraphql.composeapp.generated.resources.Res
-import cmpgraphql.composeapp.generated.resources.compose_multiplatform
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import com.khareab.cmp.graphql.di.appModule
 import com.khareab.cmp.graphql.di.sharedModule
 import com.khareab.cmp.graphql.ui.CountriesScreen
 import com.khareab.cmp.graphql.ui.CountriesViewModel
+import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.KoinApplication
 import org.koin.compose.viewmodel.koinViewModel
 
