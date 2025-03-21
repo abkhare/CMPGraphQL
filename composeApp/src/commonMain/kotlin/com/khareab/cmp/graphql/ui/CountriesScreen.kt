74260ecbaa303b00618f8d132287514c468454db
Abhiraj Khare <abhiraj.khare100@gmail.com>
2025-03-21 19:43:59 +0530

    feat: added demo video

--- composeApp/src/commonMain/kotlin/com/khareab/cmp/graphql/ui/CountriesScreen.kt
+++ composeApp/src/commonMain/kotlin/com/khareab/cmp/graphql/ui/CountriesScreen.kt
@@ -1,11 +1,13 @@
 package com.khareab.cmp.graphql.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -14,6 +16,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.khareab.cmp.graphql.domain.SimpleCountry
@@ -33,7 +36,8 @@ fun CountriesScreen(
             ) {
                 items(state.countries.size) { index ->
                     CountryItem(
-                        country = state.countries[index]
+                        country = state.countries[index],
+                        index = index
                     ) {
                         onSelectedCountry(state.countries[index].code)
                     }
@@ -48,14 +52,16 @@ fun CountriesScreen(
 fun CountryItem(
     country: SimpleCountry,
     modifier: Modifier = Modifier,
+    index: Int = 0,
     onItemClick: () -> Unit
 ) {
     Row(
         modifier = modifier
+            .fillMaxWidth()
             .padding(10.dp)
             .clickable {
             onItemClick.invoke()
-        },
+        }.background( if (index%2 == 0) Color.LightGray else Color.Cyan),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
