e4879d483ae755de8c97ec5e67d902c71aa19065
Abhiraj Khare <abhiraj.khare100@gmail.com>
2025-05-15 20:49:14 +0530

    docs: made list pretty

--- composeApp/src/commonMain/kotlin/com/khareab/cmp/graphql/ui/CountriesScreen.kt
+++ composeApp/src/commonMain/kotlin/com/khareab/cmp/graphql/ui/CountriesScreen.kt
@@ -1,5 +1,6 @@
 package com.khareab.cmp.graphql.ui
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -10,12 +11,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -62,33 +66,48 @@ fun CountryItem(
     index: Int = 0,
     onItemClick: () -> Unit
 ) {
-    Row(
+    val cardColors = if (index % 2 == 0) {
+        listOf(Color(0xFFE3F2FD), Color(0xFFBBDEFB))
+    } else {
+        listOf(Color(0xFFDCEDC8), Color(0xFFC8E6C9))
+    }
+
+    Card(
         modifier = modifier
             .fillMaxWidth()
-            .padding(10.dp)
-            .clickable {
-            onItemClick.invoke()
-        }.background( if (index%2 == 0) Color.LightGray else Color.Cyan),
-        verticalAlignment = Alignment.CenterVertically
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .shadow(elevation = 4.dp, shape = RoundedCornerShape(16.dp))
+            .clickable { onItemClick.invoke() },
+        shape = RoundedCornerShape(16.dp),
+        border = BorderStroke(1.dp, Color.LightGray),
+        backgroundColor = Color.Transparent,
     ) {
-        // Country emoji
-        Text(
-            text = country.emoji,
-            fontSize = 30.sp
-        )
-        Column(
-            modifier = Modifier.padding(10.dp),
-            verticalArrangement = Arrangement.Center
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    brush = Brush.horizontalGradient(cardColors),
+                    shape = RoundedCornerShape(16.dp)
+                )
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            // Country name and capital
+            // Country emoji
             Text(
-                text = country.name,
-                fontSize = 24.sp
+                text = country.emoji,
+                fontSize = 30.sp,
+                modifier = Modifier.padding(end = 16.dp)
             )
-            Text(
-                text = country.capital,
-                fontSize = 24.sp
-            )
+            Column(
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = country.name,
+                    fontSize = 18.sp
+                )
+                Text(text = country.capital, fontSize = 14.sp)
+            }
         }
     }
 }
+
